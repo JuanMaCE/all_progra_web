e4314677e6fe634a46172ce991a90d83d5c50f15
JuanMaCE <jk120365@gmail.com>
2025-09-10 16:13:36 -0600

    img of database generated

--- core/models.py
+++ core/models.py
@@ -7,23 +7,20 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
 
-
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
-
-
+    
 class Capitulo(models.Model):
     titulo = models.CharField(max_length=200)
     libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.titulo} ({self.libro.titulo})"
-
-
+    
 class Pagina(models.Model):
     numero = models.IntegerField()
     contenido = models.TextField()
